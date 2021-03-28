6d8d2535271da23a80e07fedc4f4401aac605879
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-28 16:05:28 -0400

    Added searchCity function, added in additional callbacks for current weather, added event listener to search button.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
 var apiKey = "8f2df02f88c96d01519b05c729d9fa02"
+var searchBtn = document.querySelector("#search-button");
 var currentDisplay = document.querySelector("#current-weather");
 
 
+function searchCity() {
+    var searchCity = document.querySelector("#search-city").value;
+    console.log(searchCity);
+    currentWeather(searchCity);
+};
 
-function currentWeather(city) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
-    fetch(url)
+
+function currentWeather(searchCity) {
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
+
+    fetch(queryUrl)
         .then(function (response) {
             if (!response.ok) {
                 throw response.json();
@@ -17,16 +25,20 @@ function currentWeather(city) {
 
         .then(function (data) {
             console.log(data);
+
             var h1 = document.createElement("h1");
             h1.textContent = data.name;
-            
+
+            var temp = document.createElement("temp");
+            temp.textContent = "Temperature: " + data.main.temp;
+
+            var humidity = document.createElement("humidity");
+            humidity.textContent = "Humidity: " + data.main.humidity;
+
             // Do callback for other API function here, because data is where I have lat./long.
+            currentDisplay.append(h1, temp, humidity);
+        })
 
+};
 
-
-            currentDisplay.append(h1);
-
-        });
-}
-
-currentWeather("Seattle");
+searchBtn.addEventListener("click", searchCity);
