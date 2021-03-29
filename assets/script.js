8ce152f6fc672d07bcaa2567de1988d7b5a8bd34
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-28 20:13:47 -0400

    Renamed some variables for easier naming convention, added in search history class

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 var apiKey = "8f2df02f88c96d01519b05c729d9fa02"
 var searchBtn = document.querySelector("#search-button");
 var currentDisplay = document.querySelector("#current-weather");
+var searchHistory = document.querySelector(".search-history")
+// var searchCity = document.querySelector("#search-city");
 
 
 function searchCity() {
     var searchCity = document.querySelector("#search-city").value;
     console.log(searchCity);
     currentWeather(searchCity);
+
+    //  localStorage.setItem(searchCity, data);
 };
 
 
@@ -16,18 +20,14 @@ function currentWeather(searchCity) {
 
     fetch(queryUrl)
         .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-
             return response.json();
         })
 
         .then(function (data) {
             console.log(data);
 
-            var h1 = document.createElement("h1");
-            h1.textContent = data.name;
+            var h2 = document.createElement("h2");
+            h2.textContent = data.name + " (" + moment().format("l") + ")";
 
             var temp = document.createElement("temp");
             temp.textContent = "Temperature: " + data.main.temp;
@@ -35,9 +35,16 @@ function currentWeather(searchCity) {
             var humidity = document.createElement("humidity");
             humidity.textContent = "Humidity: " + data.main.humidity;
 
+            // var uvIndex = document.createElement("uvIndex");
+            // uvIndex.textContent = "UV Index: " + 
+
             // Do callback for other API function here, because data is where I have lat./long.
-            currentDisplay.append(h1, temp, humidity);
+            currentDisplay.append(h2, temp, humidity);
+
+            // localStorage.setItem(searchCity, [h2, temp, humidity);
         })
+
+
 
 };
 
